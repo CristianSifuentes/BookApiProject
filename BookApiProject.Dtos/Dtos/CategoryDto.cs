ce583c2d998b3b26e3d39a0f2f7a9a749bdc01de
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2020-06-14 16:39:04 -0500

    Se sube el archivo faltante

--- BookApiProject.Dtos/Dtos/CategoryDto.cs
+++ BookApiProject.Dtos/Dtos/CategoryDto.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace BookApiProject.Dtos.Dtos
 {
-    public class CountryDto
+    public class CategoryDto
     {
         public int Id { get; set; }
         public string Name { get; set; }
